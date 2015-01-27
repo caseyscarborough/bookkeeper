bc5eddef36f3953fbeae7cc4f467b5a60a36e7d8
Casey Scarborough <caseyscarborough@gmail.com>
2015-01-26 19:02:54 -0500

    Consolidate controller actions

--- grails-app/controllers/com/caseyscarborough/budget/AccountController.groovy
+++ grails-app/controllers/com/caseyscarborough/budget/AccountController.groovy
@@ -1,16 +1,48 @@
 package com.caseyscarborough.budget
 
+import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 import grails.transaction.Transactional
+import org.springframework.http.HttpStatus
 
 @Secured('IS_AUTHENTICATED_FULLY')
 @Transactional(readOnly = true)
 class AccountController {
 
+  def accountService
   def springSecurityService
+
+  static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
   def index() {
     def accountList = Account.findAllByUser(springSecurityService.currentUser)
     [accountList: accountList, accountListCount: accountList.size(), accountTypes: AccountType.findAll()?.sort { it.name }]
+  }
+
+  @Transactional
+  def save() {
+    try {
+      def account = accountService.createAccount(params.description, new BigDecimal(params.balance), AccountType.valueOf(params.type), springSecurityService.currentUser)
+      response.status = HttpStatus.CREATED.value()
+      render account as JSON
+    } catch (AccountException e) {
+      response.status = HttpStatus.BAD_REQUEST.value()
+      render([message: e.message, field: e.account.errors.fieldError.field] as JSON)
+    } catch (NumberFormatException e) {
+      response.status = HttpStatus.BAD_REQUEST.value()
+      render([message: "Please enter a valid balance for the account.", field: "balance"] as JSON)
+    }
+  }
+
+  def edit(Account accountInstance) {
+    respond accountInstance
+  }
+
+  @Transactional
+  def delete(Long id) {
+    def accountInstance = Account.get(id)
+    accountInstance.delete(flush: true)
+    response.status = HttpStatus.NO_CONTENT.value()
+    render([] as JSON)
   }
 }
--- grails-app/controllers/com/caseyscarborough/budget/TransactionController.groovy
+++ grails-app/controllers/com/caseyscarborough/budget/TransactionController.groovy
@@ -1,13 +1,19 @@
 package com.caseyscarborough.budget
 
+import grails.converters.JSON
 import grails.plugin.springsecurity.annotation.Secured
 import grails.transaction.Transactional
+import org.springframework.http.HttpStatus
+
+import java.text.ParseException
 
 @Secured('IS_AUTHENTICATED_FULLY')
 @Transactional(readOnly = true)
 class TransactionController {
 
+  def transactionService
   def springSecurityService
+
   static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
   def index(Integer max) {
@@ -15,5 +21,42 @@ class TransactionController {
     def transactions = Transaction.findAllByUser(springSecurityService.currentUser, params)
     def accounts = Account.findAllByUser(springSecurityService.currentUser)
     [transactions: transactions, transactionInstanceCount: Transaction.count(), accounts: accounts, categories: Category.all]
+  }
+
+  @Transactional
+  def save() {
+    try {
+      def transaction = transactionService.createTransaction(
+          params.description, new BigDecimal(params.amount), Account.get(params.account), SubCategory.get(params.subCategory), Date.parse("MM/dd/yyyy", params.date), springSecurityService.currentUser)
+      response.status = HttpStatus.CREATED.value()
+      render transaction as JSON
+    } catch (TransactionException e) {
+      response.status = HttpStatus.BAD_REQUEST.value()
+      render([message: e.message, field: e.transaction.errors.fieldError.field] as JSON)
+    } catch (NumberFormatException e) {
+      response.status = HttpStatus.BAD_REQUEST.value()
+      render([message: "Please enter a valid amount for the transaction.", field: "transaction"] as JSON)
+    } catch (ParseException e) {
+      response.status = HttpStatus.BAD_REQUEST.value()
+      render([message: "Please enter a valid date in the format YYYY-MM-DD.", field: "date"] as JSON)
+    }
+  }
+
+  @Transactional
+  def delete(Long id) {
+    def transactionInstance = Transaction.get(id)
+    transactionService.deleteTransaction(transactionInstance)
+    response.status = HttpStatus.NO_CONTENT.value()
+    render([] as JSON)
+  }
+
+  def queryDescription() {
+    def transactions = Transaction.findAllByUserAndDescriptionIlike(springSecurityService.currentUser, params.query + "%")
+    def output = []
+    transactions.each { t ->
+      output << [ value: t.description, data: t.subCategory.id ]
+    }
+    output = output.unique()
+    render([suggestions: output] as JSON)
   }
 }

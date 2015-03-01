788077cfa432e36a0498be8375ae53c1823c10bd
Casey Scarborough <caseyscarborough@gmail.com>
2015-03-01 15:50:02 -0500

    Add Receipt domain class.

---
+++ grails-app/domain/com/caseyscarborough/budget/Receipt.groovy
@@ -0,0 +1,14 @@
+package com.caseyscarborough.budget
+
+class Receipt {
+
+  String location
+  String filename
+  Long size
+  String contentType
+
+  Transaction transaction
+
+  static constraints = {
+  }
+}

0d3b30764990716a32768dc0a2210465f396c36d
Casey Scarborough <caseyscarborough@gmail.com>
2015-03-07 10:47:39 -0500

    Replace const with var for ES5 compatibility.

--- grails-app/assets/javascripts/urls.js
+++ grails-app/assets/javascripts/urls.js
@@ -1,5 +1,5 @@
-const CONTEXT_PATH = "/budget";
-const TRANSACTION_CREATE_URL = CONTEXT_PATH + "/transaction/save";
-const TRANSACTION_DELETE_URL = CONTEXT_PATH + "/transaction/delete";
-const TRANSACTION_UPDATE_URL = CONTEXT_PATH + "/transaction/update";
-const ACCOUNT_UPDATE_URL = CONTEXT_PATH + "/account/update";
+var CONTEXT_PATH = "/budget";
+var TRANSACTION_CREATE_URL = CONTEXT_PATH + "/transaction/save";
+var TRANSACTION_DELETE_URL = CONTEXT_PATH + "/transaction/delete";
+var TRANSACTION_UPDATE_URL = CONTEXT_PATH + "/transaction/update";
+var ACCOUNT_UPDATE_URL = CONTEXT_PATH + "/account/update";
